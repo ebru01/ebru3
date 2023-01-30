5fec6754e239dc7a3e0c313cdde9fedbbfe586b0
ebru01 <kanepec9@gmail.com>
2023-01-30 17:28:14 +0100

    task 3 yapildi

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -3,5 +3,7 @@ package Gun01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
+        System.out.println("task 1");
+        System.out.println("task 3");
     }
 }
